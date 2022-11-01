4243a48197ae1e0e11c1184bda1d2262196ecd25
Luke Bullen <lukeabullen@gmail.com>
2022-11-01 17:09:55 +0000

    allow only 1 answer clicked in quiz - not styled properly

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -55,23 +55,37 @@ export default class extends Controller {
     const button = this.buttonTarget;
     if (this.correctValue == "correct") {
         console.log("xzxz");
-        // document.getElementsByClassName("quizz-btnzz").disabled = true;
         const bnz = document.getElementsByClassName("quizz-btnzz");
-        // bnz.all.disabled = true;
         console.log(bnz);
         for(var i = 0; i < bnz.length; i++) {
           bnz[i].disabled = true;
-        }
+        };
+
+
+        button.style.backgroundColor = "#3e753b"; //green
+        const quizResultMessage = document.getElementById("quiz-result-message");
+        quizResultMessage.innerText = "Congratulations! You won a new pack of 5 cards!";
+        quizResultMessage.style.backgroundColor = "rgba(62, 117, 59, 0.7)"; //green
+        quizResultMessage.style.display = "block";
+        const quizOpenPack = document.getElementById("quiz-open-pack").style.display = "block";
 
     } else {
         console.log("hjsjd");
-        // document.getElementsByClassName("quizz-btnzz").disabled = true;
         const bnz = document.getElementsByClassName("quizz-btnzz");
-        // bnz.all.disabled = true;
         console.log(bnz);
         for(var i = 0; i < bnz.length; i++) {
           bnz[i].disabled = true;
-        }
+        };
+
+
+
+        button.style.backgroundColor = "#C8102E"; //$red
+        const quizResultMessage = document.getElementById("quiz-result-message");
+        quizResultMessage.innerText = "Sooooo cloze!!! Best luck next time";
+        quizResultMessage.style.backgroundColor = "rgba(200, 16, 46, 0.7)"; //$red
+        quizResultMessage.style.display = "block";
+        document.getElementById("quiz-try-another").style.display = "block";
+
     };
 };
 }
